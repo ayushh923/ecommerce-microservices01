8b1a3078a350302f2cec2b5972bdb48857e27170
ayushh923 <ayushh923@gmail.com>
2025-12-10 11:06:55 +0530

    Update ResponseMessage.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/response/ResponseMessage.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/response/ResponseMessage.java
@@ -1,14 +1,14 @@
 package com.hoangtien2k3.userservice.model.dto.response;
 
 import lombok.*;
-
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Size;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 public class ResponseMessage {
-    @Size(min = 10, max = 500)
+
+    @Size(min = 10, max = 500, message = "Message must be between 10 and 500 characters")
     private String message;
 }
