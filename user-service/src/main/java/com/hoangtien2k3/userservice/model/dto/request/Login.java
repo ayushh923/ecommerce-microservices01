7d75d398e1b2faa6da830360214b5597be0d6ddd
ayushh923 <ayushh923@gmail.com>
2025-12-10 11:14:01 +0530

    Update Login.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/Login.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/Login.java
@@ -1,13 +1,20 @@
 package com.hoangtien2k3.userservice.model.dto.request;
 
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class Login {
 
+    @NotBlank(message = "Username or Email cannot be blank")
     private String username;
 
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
 }
