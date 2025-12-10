d52eb4e3170d8d3b8d601af094e468e363a93ec8
ayushh923 <ayushh923@gmail.com>
2025-12-10 10:35:15 +0530

    Update UserServiceApplication.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/UserServiceApplication.java
@@ -2,14 +2,14 @@ package com.hoangtien2k3.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class  UserServiceApplication {
+@EnableDiscoveryClient
+public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
