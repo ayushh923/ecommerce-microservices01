f80b9e95b4c252c7dd0a7598230a193d14f241ef
ayushh923 <ayushh923@gmail.com>
2025-12-10 10:56:42 +0530

    Update RoleName.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/model/entity/RoleName.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/model/entity/RoleName.java
@@ -3,10 +3,14 @@ package com.hoangtien2k3.userservice.model.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+/**
+ * System roles available for application users.
+ * This enum is stored as STRING in the database (see Role.java).
+ */
 @Getter
+@RequiredArgsConstructor
 public enum RoleName {
-    USER,
-    PM,
-    ADMIN
+    USER,    // Regular user
+    PM,      // Project / Product Manager
+    ADMIN    // System administrator
 }
