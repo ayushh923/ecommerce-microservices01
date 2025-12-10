48f77ff5f2d670d3f49b36427ade0b917f56609f
ayushh923 <ayushh923@gmail.com>
2025-12-10 11:13:10 +0530

    Update ChangePasswordRequest.java

--- user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/ChangePasswordRequest.java
+++ user-service/src/main/java/com/hoangtien2k3/userservice/model/dto/request/ChangePasswordRequest.java
@@ -1,5 +1,7 @@
 package com.hoangtien2k3.userservice.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,14 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class ChangePasswordRequest {
-    String oldPassword;
-    String newPassword;
-    String confirmPassword;
+
+    @NotBlank(message = "Old password cannot be blank")
+    private String oldPassword;
+
+    @NotBlank(message = "New password cannot be blank")
+    @Size(min = 8, max = 50, message = "New password must be between 8 and 50 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password cannot be blank")
+    private String confirmPassword;
 }
